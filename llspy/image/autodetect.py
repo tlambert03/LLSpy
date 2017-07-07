010d17078bc0dd40ef4eab78b1f8d47e204693b2
Talley Lambert <talley.lambert@gmail.com>
2017-07-07 18:09:46 -0400

    imcontent accepts 2d, feature width works with objective scan

--- llspy/image/autodetect.py
+++ llspy/image/autodetect.py
@@ -10,12 +10,13 @@ from scipy.stats import mode
 def imcontentbounds(im, sigma=2):
 	"""Get image content bounding box via gaussian filter and threshold."""
 	# get rid of the first two planes in case of high dark noise
-	b = im[2:].astype(np.float)
-	fullwidth = b.shape[-1]
-	b = np.squeeze(np.max(b, 0))
+	if im.ndim == 3:
+		im = np.squeeze(np.max(im[2:], 0))
+	im = im.astype(np.float)
+	fullwidth = im.shape[-1]
 	# from scipy.ndimage.filters import median_filter
 	# mm = median_filter(b.astype(float),3)
-	mm = b
+	mm = im
 	imgaus = gaussian(mm, sigma=sigma)
 	mask = imgaus > threshold_li(imgaus)
 	linesum = np.sum(mask, 0)
@@ -25,7 +26,7 @@ def imcontentbounds(im, sigma=2):
 	return [left, right, fullwidth]
 
 
-def feature_width(E, background=None, pad=100, sigma=2):
+def feature_width(E, background=None, pad=50, sigma=2):
 	"""automated detection of post-deskew image content width.
 
 	the width can be used during deskewing to crop the final image to
@@ -37,7 +38,10 @@ def feature_width(E, background=None, pad=100, sigma=2):
 	# first and last timepoint
 	raw_stacks = [tf.imread(f) for f in E.get_files(t=(0, P.nt - 1))]
 	raw_stacks = [sub_background(f, background) for f in raw_stacks]
-	deskewed_stacks = [deskew(s, P.dz, P.dx, P.angle) for s in raw_stacks]
+	if P.samplescan:
+		deskewed_stacks = [deskew(s, P.dz, P.dx, P.angle) for s in raw_stacks]
+	else:
+		deskewed_stacks = raw_stacks
 
 	# then get minimum bounding box of features
 	bounds = np.array([imcontentbounds(d, sigma) for d in deskewed_stacks])
