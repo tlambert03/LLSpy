5111fb9d18864e2f0503fbc3397c2be79bf7d126
Talley Lambert <talley.lambert@gmail.com>
2017-07-07 18:10:20 -0400

    reorderstack, imsave, and pyrange_to_perlregex

--- llspy/util/util.py
+++ llspy/util/util.py
@@ -1,6 +1,8 @@
 import os
 import fnmatch
-
+import warnings
+import tifffile
+import numpy as np
 
 class dotdict(dict):
 	"""dot.notation access to dictionary attributes"""
@@ -67,3 +69,40 @@ def get_subfolders_containing_filepattern(
 			if not any([e in root for e in exclude]):
 				matches.append(root)
 	return matches
+
+
+def pyrange_to_perlregex(it, digits=4):
+	L = []
+	for i in it:
+		L.append(str(i).zfill(digits))
+	return str("(" + "|".join(L) + ")")
+
+
+def reorderstack(arr, inorder, outorder='tzcyx'):
+	"""rearrange order of array, used when resaving a file."""
+	inorder = inorder.lower()
+	for _ in range(len(outorder) - arr.ndim):
+		arr = np.expand_dims(arr, 0)
+	for i in outorder:
+		if i not in inorder:
+			inorder = i + inorder
+	arr = np.transpose(arr, [inorder.find(n) for n in outorder])
+	return arr
+
+
+def imsave(arr, outpath, dx=1, dz=1, dt=1, unit='micron'):
+	"""sample wrapper for tifffile.imsave imagej=True."""
+	# array must be in TZCYX order
+	md = {
+		'unit': unit,
+		'spacing': dz,
+		'finterval': dt,
+		'hyperstack': 'true',
+		'mode': 'composite',
+		'loop': 'true',
+	}
+	bigT = True if arr.nbytes > 3758096384 else False  # > 3.5GB make a bigTiff
+	with warnings.catch_warnings():
+		warnings.simplefilter("ignore")
+		tifffile.imsave(outpath, arr, bigtiff=bigT, imagej=True,
+						resolution=(1 / dx, 1 / dx), metadata=md)
