2b1e2779a61f52389807139154219caf98b86c71
Talley Lambert <talley.lambert@gmail.com>
2017-07-30 16:18:33 -0400

    slight updates to __init__ files

--- llspy/__init__.py
+++ llspy/__init__.py
@@ -4,6 +4,7 @@ import logging
 logging.basicConfig(format='%(levelname)s:%(name)s | %(message)s')
 logger = logging.getLogger(__name__)
 
+
 try:
 	import pathlib as plib
 	plib.Path().expanduser()
@@ -12,15 +13,25 @@ except (ImportError, AttributeError):
 except (ImportError, AttributeError):
 	raise ImportError('no pathlib detected. For python2: pip install pathlib2')
 
-from llspy.config import config
 
-from llspy.core.llsdir import LLSdir
+
+from llspy.config import config
+from llspy.core.llsdir import LLSdir, preview, process
+from llspy.core import otf
 from llspy.core.settingstxt import LLSsettings as Settxt
 from llspy.core.cudabinwrapper import CUDAbin
-from llspy.core.parse import parse_filename
-
+from llspy.core.parse import parse_filename, filter_files
 from llspy.samples import samples
-
 from llspy.camera.camera import CameraROI, CameraParameters, correctInsensitivePixels
-
 from llspy import image
+from llspy.core.schema import procParams, printOptions
+
+#libcuda functions
+try:
+	from llspy.core.libcudawrapper import deskewGPU as deskew
+	from llspy.core.libcudawrapper import affineGPU as affine
+	from llspy.core.libcudawrapper import quickDecon as decon
+	from llspy.core.libcudawrapper import rotateGPU as rotate
+	from llspy.core.libcudawrapper import quickCamcor as camcor
+except Exception:
+	pass
--- llspy/image/__init__.py
+++ llspy/image/__init__.py
@@ -1,4 +1,2 @@
-# note: numba.cuda MUST be imported before gputools, otherwise segfault 11
-from .deskew import deskew
 from .register import calcTranslationRegistration
-from .autodetect import feature_width
+from .arrayfun import feature_width
