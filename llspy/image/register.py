74f2d98d69a1a64c5c8c8f3c4b3e820c8ba283b9
Talley Lambert <talley.lambert@gmail.com>
2017-07-30 16:30:40 -0400

    improved point matching across channels

--- llspy/image/register.py
+++ llspy/image/register.py
@@ -1,5 +1,5 @@
 from __future__ import print_function, division
-from tifffile import imread, imshow
+from tifffile import imread
 import numpy as np
 from scipy import ndimage
 from scipy import stats
@@ -12,14 +12,8 @@ except Exception:
 	print("could not import SimpleITK module!")
 	__sitkImported = False
 
-try:
-	import cv2
-	__cv2Imported = True
-except Exception:
-	print("could not import opencv module!")
-	__cv2Imported = False
 
-
+## THIS FILE NOT REALLY USED AT THE MOMENT...  SEE FIDUCIALREG MODULE ##
 def calcTranslationRegistration(moving, fixed):
 	"""calculate the translation shift between two images"""
 
@@ -104,15 +98,48 @@ def autodetect_peaks(ims, minparticles=4, threshrange=range(200, 520, 20)):
 	else:
 		return peaks[0]
 
-def normxcorr2(b,a):
+
+def normxcorr2(b, a):
 	c = scipy.signal.convolve2d(a, np.flipud(np.fliplr(b)))
 	a = scipy.signal.convolve2d(a**2, np.ones_like(b))
 	b = np.sum(b.flatten()**2)
-	return c/sqrt(a*b)
+	return c / np.sqrt(a * b)
 
+
+def get_closest_points(pc1, pc2):
+	"""returns the distance and index of the closest matching point in pc1
+	for each point in pc2.
+
+	len(nn) == len(pc1)
+
+	can be used to eliminate points in pc1 that don't have a partner in pc2
+	"""
+	d = [((pc2 - point)**2).sum(axis=1) for point in pc1]
+	nn = [(np.min(p), np.argmin(p)) for p in d]
+	return nn
+
+
+def get_matching_points(pc1, pc2, maxd=100):
+	""" return modified point clouds such that every point in pc1 has a
+	neighbor in pc2 that is within distance maxd
+	"""
+	nn12 = np.array(get_closest_points(pc1, pc2))
+	pc1 = pc1[np.array([n[0] < 100 for n in nn12])]
+
+	nn21 = np.array(get_closest_points(pc2, pc1))
+	pc2 = pc2[np.array([n[0] < 100 for n in nn21])]
+
+	return pc1, pc2
 
 
 if __name__ == '__main__':
+	try:
+		import cv2
+		__cv2Imported = True
+	except Exception:
+		print("could not import opencv module!")
+		__cv2Imported = False
+
 	img1 = imread('/Users/talley/DropboxHMS/CBMF/lattice_sample_data/lls_registration_samp/janelia_reg_example/deskewed/488_beadfield_0,6umStep_deskewed.tif')
 	img2 = imread('/Users/talley/DropboxHMS/CBMF/lattice_sample_data/lls_registration_samp/janelia_reg_example/deskewed/560_beadfield_0,6umStep_uncorrected_deskewed.tif')
 
@@ -140,9 +167,10 @@ if __name__ == '__main__':
 	temp = img2sum.astype(np.float32)
 	crop = 20
 	temp = temp[crop:-crop, crop:-crop]
-	result = cv2.matchTemplate(img1sum.astype(np.float32), temp, cv2.TM_CCORR_NORMED)
-	min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-	xshift = crop - max_loc[0]
-	yshift = crop - max_loc[1]
-	print(xshift)
-	print(yshift)
+	if __cv2Imported:
+		result = cv2.matchTemplate(img1sum.astype(np.float32), temp, cv2.TM_CCORR_NORMED)
+		min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+		xshift = crop - max_loc[0]
+		yshift = crop - max_loc[1]
+		print(xshift)
+		print(yshift)
