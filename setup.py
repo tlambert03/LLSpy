b190dfbaa08796a96af7c7be3267263749ee96ec
Talley Lambert <talley.lambert@gmail.com>
2017-09-13 17:32:04 -0400

    include libfftw for windows

--- setup.py
+++ setup.py
@@ -1,60 +1,57 @@
 # -*- coding: utf-8 -*-
-
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
 import sys
 
-here = path.abspath(path.dirname(__file__))
+INCLUDELIBS = False
+HERE = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    readme = f.read()
+with open(path.join(HERE, 'README.rst'), encoding='utf-8') as f:
+    README = f.read()
 
 with open('LICENSE.txt') as f:
-    license = f.read()
+    LICENSE = f.read()
 
-# get specific library by platform
-if sys.platform.startswith('darwin'):
-    PLATFORM = 'darwin'
-    PACKAGE_DATA = [
-            path.join('bin', 'cudaDeconv.app'),
-            path.join('bin', 'otfviewer.app'),
-            path.join('bin', 'radialft.app'),
-            path.join('lib', 'libcudaDeconv.dylib'),
-            path.join('lib', 'libradialft.dylib'),
-    ]
-elif sys.platform.startswith('win32'):
-    PLATFORM = 'win32'
-    PACKAGE_DATA = [
-            path.join('bin', 'cudaDeconv.exe'),
-            path.join('bin', 'otfviewer.exe'),
-            path.join('bin', 'radialft.exe'),
-            path.join('bin', 'libfftw3f-3.dll'),
-            path.join('lib', 'libcudaDeconv.dll'),
-            path.join('lib', 'libradialft.dll'),
-    ]
-else:
-    PLATFORM = 'nix'
-    PACKAGE_DATA = [
-            path.join('bin', 'cudaDeconv'),
-            path.join('bin', 'otfviewer'),
-            path.join('bin', 'radialft'),
-            path.join('lib', 'libcudaDeconv.so'),
-            path.join('lib', 'libradialft.so'),
-    ]
 
-PACKAGE_DATA = []
+PACKAGE_DATA = ['gui/guiDefaults.ini']
+
+
+if sys.platform.startswith('win32'):
+    PACKAGE_DATA += 'bin/libfftw3f-3.dll'
+
+
+if INCLUDELIBS:
+    # add specific library by platform
+    if sys.platform.startswith('darwin'):
+        PACKAGE_DATA += [
+                'bin/*.app',
+                'lib/*.dylib',
+        ]
+    elif sys.platform.startswith('win32'):
+        PACKAGE_DATA += [
+                'bin/*.exe',
+                'lib/*.dll',
+        ]
+    else:
+        PACKAGE_DATA += [
+                'bin/cudaDeconv',
+                'bin/otfviewer',
+                'bin/radialft',
+                'lib/*.so',
+        ]
+
 
 setup(
     name='llspy',
     version='0.1.0',
     description='Lattice Light Sheet Processing Tools',
-    long_description=readme,
+    long_description=README,
     author='Talley Lambert',
     author_email='talley.lambert@gmail.com',
     url='https://github.com/tlambert03/LLSpy2',
-    license=license,
+    license=LICENSE,
     packages=find_packages(exclude=('tests', 'docs')),
     python_requires='>=3.5',
     package_data={
