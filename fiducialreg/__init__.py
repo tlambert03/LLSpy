6961e1bd627f441e89f6658bc6c9e3afb1da4e6c
Talley Lambert <talley.lambert@gmail.com>
2017-09-19 10:02:59 -0400

    version back to 0.1.0

--- fiducialreg/__init__.py
+++ fiducialreg/__init__.py
@@ -4,6 +4,6 @@ from . import imref
 from . import imwarp
 
 
-__version__ = '0.10.0'
+__version__ = '0.1.0'
 __all__ = ('GaussFitter3D', 'FiducialCloud', 'rigid_registration',
 	'affine_registration')
