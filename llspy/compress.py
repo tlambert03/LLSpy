50aa1ebc0f347aeaf3a85daf9d08aaf4b8712ba3
Talley Lambert <talley.lambert@gmail.com>
2017-09-23 14:36:08 -0400

    partial decomp, don't compress when tar exists

--- llspy/compress.py
+++ llspy/compress.py
@@ -1,11 +1,13 @@
 import tarfile
 import os
+import sys
 import subprocess
 from . import util
 
 EXTENTIONS = {
 	'.bz2': 'lbzip2',
 	'.gz': 'pigz',
+	'.zz': 'pigz',
 }
 
 archive_extension = {
@@ -15,6 +17,10 @@ archive_extension = {
 	'pigz': '.gz',
 	'gzip': '.gz',
 }
+
+
+def get_platform_compression():
+	return 'pigz' if sys.platform.startswith("win32") else 'lbzip2'
 
 
 def tartiffs(path, delete=True):
@@ -53,7 +59,7 @@ def untar(tarball, delete=True):
 
 def zipit(fname, compression=None):
 	if compression is None:
-		compression = 'lbzip2'
+		compression = get_platform_compression()
 	# check if it exists and is not already compressed
 	assert os.path.exists(fname), 'File does not exist: {}'.format(fname)
 	assert os.path.splitext(fname)[1] not in (archive_extension[compression],), 'File already compressed: ' + fname
@@ -69,6 +75,7 @@ def unzipit(fname, compression=None):
 	extension = os.path.splitext(fname)[1]
 	if compression is None:
 		compression = EXTENTIONS[extension]
+	print("zipping with compression: ", compression)
 	assert archive_extension[compression] == extension, "Format {} cannot be unzipped by program {}".format(extension, compression)
 	# check if it exists and is compressed type
 	assert os.path.exists(fname), 'File does not exist: {}'.format(fname)
@@ -77,16 +84,61 @@ def unzipit(fname, compression=None):
 	return fname.strip(archive_extension[compression])
 
 
+def unzip_partial(fname, tRange=None, compression=None):
+	if tRange is None:
+		tRange = [0]
+	extension = os.path.splitext(fname)[1]
+	if compression is None:
+		compression = EXTENTIONS[extension]
+	assert archive_extension[compression] == extension, "Format {} cannot be unzipped by program {}".format(extension, compression)
+	# check if it exists and is compressed type
+	assert os.path.exists(fname), 'File does not exist: {}'.format(fname)
+	assert extension in (archive_extension[compression],), 'File not compressed: ' + fname
+
+	try:
+		cmd = ['tar', 'xf', fname, '-C', os.path.dirname(fname), '--wildcards']
+		cmd.extend(['*stack{:04d}*'.format(f) for f in tRange])
+		cmd.extend(['--use-compress-program', compression])
+		subprocess.call(cmd)
+	except Exception:
+		files_i_want = ['stack{:04d}'.format(f) for f in tRange]
+		with tarfile.open(fname) as tar:
+			tar.extractall(path=os.path.dirname(fname),
+				members=[x for x in tar.getmembers() if any(S in x.name for S in files_i_want)])
+
+
 def compress(path, compression=None):
+	if util.find_filepattern(path, '*.tar*') is not None:
+		raise("There is already a compressed file in this directory")
 	tar = tartiffs(path)
 	return zipit(tar, compression) if tar is not None and os.path.isfile(tar) else None
 
 
 def decompress(file, compression=None):
+	if compression is None:
+		compression = get_platform_compression()
 	# if it's not a tar.bz2, assume it's a directory that contains one
 	compressedtar = util.find_filepattern(file, '*.tar*') if os.path.isdir(file) else file
-	if compressedtar is None or not compressedtar.endswith(archive_extension[compression]):
+	if compressedtar is None:
 		print('No compressed files found in ' + file)
+		return None
+	elif not compressedtar.endswith(archive_extension[compression]):
+		print('Cannot decompress {} with program {} '.format(compressedtar, compression))
 		return None
 	tarball = unzipit(compressedtar, compression)
 	return untar(tarball)
+
+
+def decompress_partial(file, tRange, compression=None):
+	if compression is None:
+		compression = get_platform_compression()
+	# if it's not a tar.bz2, assume it's a directory that contains one
+	compressedtar = util.find_filepattern(file, '*.tar*') if os.path.isdir(file) else file
+	if compressedtar is None:
+		print('No compressed files found in ' + file)
+		return None
+	elif not compressedtar.endswith(archive_extension[compression]):
+		print('Cannot decompress {} with program {} '.format(compressedtar, compression))
+		return None
+	unzip_partial(compressedtar, tRange, compression)
+
