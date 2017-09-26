d65ba55ae77368bdc7610731225ab2e763a2dd0f
Talley Lambert <talley.lambert@gmail.com>
2017-09-26 17:02:59 -0400

    eventually may use to style main gui

---
+++ llspy/gui/styles.py
@@ -0,0 +1,17 @@
+from PyQt5 import QtWidgets, QtGui, QtCore
+
+
+# first use APP.setStyle(QtW.QStyleFactory.create("fusion"))
+DarkPalette = QtGui.QPalette()
+DarkPalette.setColor(QtGui.QPalette.Window, QtGui.QColor(53, 53, 53))
+DarkPalette.setColor(QtGui.QPalette.WindowText, QtCore.Qt.lightGray)
+DarkPalette.setColor(QtGui.QPalette.Base, QtGui.QColor(15, 15, 15))
+DarkPalette.setColor(QtGui.QPalette.AlternateBase, QtGui.QColor(53, 53, 53))
+DarkPalette.setColor(QtGui.QPalette.ToolTipBase, QtCore.Qt.lightGray)
+DarkPalette.setColor(QtGui.QPalette.ToolTipText, QtCore.Qt.lightGray)
+DarkPalette.setColor(QtGui.QPalette.Text, QtCore.Qt.gray)
+DarkPalette.setColor(QtGui.QPalette.Button, QtGui.QColor(53, 53, 53))
+DarkPalette.setColor(QtGui.QPalette.ButtonText, QtCore.Qt.gray)
+DarkPalette.setColor(QtGui.QPalette.BrightText, QtCore.Qt.red)
+DarkPalette.setColor(QtGui.QPalette.Highlight, QtGui.QColor(142, 45, 197).lighter())
+DarkPalette.setColor(QtGui.QPalette.HighlightedText, QtCore.Qt.black)
