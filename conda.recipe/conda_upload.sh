5ff18e66a4701ded5dd7cd0017a9762ebe6ea5cb
Talley Lambert <talley.lambert@gmail.com>
2017-10-01 22:25:30 -0400

    travis again

--- conda.recipe/conda_upload.sh
+++ conda.recipe/conda_upload.sh
@@ -8,10 +8,5 @@ conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION=`date +%Y.%m.%d`
 conda build .
-echo $CONDA_BLD_PATH
-ls $CONDA_BLD_PATH
-ls ~/conda-bld
-echo $OS
-ls $CONDA_BLD_PATH/$OS
 
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-0.tar.bz2 --force
