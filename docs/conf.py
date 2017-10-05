93b5c0bc77d58d3f16332d2442a22abe0bed720c
Talley Lambert <talley.lambert@gmail.com>
2017-10-05 23:39:38 +0200

    add voluptuous.humanize to mocks

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ class Mock(MagicMock):
 MOCK_MODULES = ['gputools', 'spimagine', 'pyopencl', 'pyopengl', 'numpy',
                 'matplotlib', 'scipy', 'scipy.ndimage.filters', 'scipy.stats',
                 'tifffile', 'PyQt5', 'raven', 'watchdog', 'numba', 'click',
-                'voluptuous']
+                'voluptuous', 'voluptuous.humanize']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
