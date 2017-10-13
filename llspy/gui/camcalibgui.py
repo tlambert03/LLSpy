f714be736cd2758931798b0e41f9e405a2ff5322
Talley Lambert <talley.lambert@gmail.com>
2017-10-12 23:39:33 -0400

    refactored camcorgui to detect and use dark images in folder

--- llspy/gui/camcalibgui.py
+++ llspy/gui/camcalibgui.py
@@ -2,10 +2,11 @@ from PyQt5 import QtCore, QtGui
 from PyQt5 import QtWidgets as QtW
 import llspy
 from llspy import camcalib
-from llspy.util import getAbsoluteResourcePath
+from llspy.util import getAbsoluteResourcePath, pathHasPattern
 import numpy as np
 import os
 import sys
+import glob
 import tifffile as tf
 from llspy.gui.camcordialog import Ui_Dialog as camcorDialog
 from llspy.gui.helpers import newWorkerThread
@@ -17,58 +18,54 @@ class CamCalibWorker(QtCore.QObject):
     """docstring for TimePointWorker"""
 
     finished = QtCore.pyqtSignal()
-    progress = QtCore.pyqtSignal()
+    progress = QtCore.pyqtSignal(int)
     setProgMax = QtCore.pyqtSignal(int)
     setStatus = QtCore.pyqtSignal(str)
     error = QtCore.pyqtSignal()
 
-    def __init__(self, folder, darkAVG, darkSTD=None):
+    def __init__(self, folder, darkavg=None, darkstd=None):
         super(CamCalibWorker, self).__init__()
         self.folder = folder
-        self.darkAVG = darkAVG
-        self.darkSTD = darkSTD
+        self.darkavg = darkavg
+        self.darkstd = darkstd
 
     @QtCore.pyqtSlot()
     def work(self):
 
+        def updatedarkstatus(prog):
+            if prog == 0:
+                self.setStatus.emit('Calculating offset map... [Step 2 of 4]')
+            elif prog == 0:
+                self.setStatus.emit('Calculating noise map... [Step 3 of 4]')
+            return
+
         try:
-            self.setStatus.emit('Loading tiff files... progress bar output will begin in a minute')
-            ch0list, ch1list = camcalib.get_channel_list(self.folder)
-            pre, post = camcalib.combine_stacks(ch0list, ch1list, self.darkAVG)
-            nz, ny, nx = pre.shape
+            # first handle dark images
+            self.setStatus.emit('Loading dark images... [Step 1 of 4]')
+            darklist = glob.glob(os.path.join(self.folder, '*dark*.tif'))
+            numdark = len(darklist)
+            self.setProgMax.emit(numdark*2)
+            darkavg = self.darkavg
+            darkstd = self.darkstd
+            if not (darkavg and darkstd):
+                darkavg, darkstd = camcalib.process_dark_images(
+                            self.folder, self.progress.emit, updatedarkstatus)
+
+            with tf.TiffFile(darklist[0]) as t:
+                nz, ny, nx = t.series[0].shape
+
             self.setProgMax.emit(ny*nx)
-
             self.setStatus.emit('Calculating correction image... This will take a while')
-            results = camcalib.parallel_fit(pre, post, self.progress.emit)
+            out = camcalib.process_bright_images(self.folder, darkavg, darkstd, self.progress.emit)
 
-            # add dark images to the results
-            results = np.vstack((results, self.darkSTD[None, :, :]))
-            if self.darkSTD is not None:
-                results = np.vstack((results, self.darkSTD[None, :, :]))
+            self.setStatus.emit("Done! Calibration file has been written to: {}".format(out[0]))
 
-            # create name
-            E = llspy.LLSdir(self.folder, ditch_partial=False)
-            outname = "FlashParam_sn{}_roi{}_date{}.tif".format(
-                E.settings.camera.serial,
-                "-".join([str(i) for i in E.settings.camera.roi]),
-                E.date.strftime('%Y%m%d'))
-
-            # reorder and write
-            results = llspy.util.reorderstack(results, 'zyx').astype(np.float32)
-            tf.imsave(os.path.join(self.folder, outname), results, imagej=True,
-                resolution=(1 / E.parameters.dx, 1 / E.parameters.dx),
-                metadata={
-                    'unit': 'micron',
-                    'hyperstack': 'true',
-                    'mode': 'composite'})
-
-            self.setStatus.emit('Calibration Tiff written to input folder.  You may close this dialog.')
             self.finished.emit()
 
         except Exception:
-            (excepttype, value, traceback) = sys.exc_info()
-            sys.excepthook(excepttype, value, traceback)
             self.error.emit()
+            self.finished.emit()
+            raise
 
 
 class CamCalibDialog(QtW.QDialog, camcorDialog):
@@ -84,10 +81,16 @@ class CamCalibDialog(QtW.QDialog, camcorDialog):
 
         self.selectFolderPushButton.clicked.connect(self.setFolder)
 
-        self.choseDarkImgPushButton.clicked.connect(lambda:
-            self.darkImageLineEdit.setText(
+        self.DarkAVGPushButton.clicked.connect(lambda:
+            self.darkAVGLineEdit.setText(
                 QtW.QFileDialog.getOpenFileName(
                     self, 'Chose Dark_AVG.tif', '',
+                    "Image Files (*.tif *.tiff)")[0]))
+
+        self.DarkSTDPushButton.clicked.connect(lambda:
+            self.darkSTDLineEdit.setText(
+                QtW.QFileDialog.getOpenFileName(
+                    self, 'Chose Dark_STD.tif', '',
                     "Image Files (*.tif *.tiff)")[0]))
 
         self.runButton.clicked.connect(self.processFolder)
@@ -100,33 +103,57 @@ class CamCalibDialog(QtW.QDialog, camcorDialog):
         folder = self.camCalibFolderLineEdit.text()
 
         if os.path.isfile(os.path.join(folder, 'dark_AVG.tif')):
-            self.darkImageLineEdit.setText(os.path.join(folder, 'dark_AVG.tif'))
+            self.darkAVGLineEdit.setText(os.path.join(folder, 'dark_AVG.tif'))
+        if os.path.isfile(os.path.join(folder, 'dark_STD.tif')):
+            self.darkSTDineEdit.setText(os.path.join(folder, 'dark_STD.tif'))
 
     def processFolder(self):
 
         folder = self.camCalibFolderLineEdit.text()
-        # futhermore ... there should be a folder inside of that called 'dark' that
-        # holds the following files:
-        # '.\dark\dark_AVG.tif'  -> an Avgerage projection of > 20,000 dark images
-        # '.\dark\dark_STD.tif'  -> an StdDev projection of > 20,000 dark images
-        darkavg = tf.imread(os.path.join(folder, 'dark_AVG.tif'))
-        if os.path.isfile(os.path.join(folder, 'dark_STD.tif')):
-            darkstd = tf.imread(os.path.join(folder, 'dark_STD.tif'))
-        else:
-            darkstd = None
 
-        self.worker, self.thread = newWorkerThread(CamCalibWorker, folder, darkavg, darkstd,
+        darkavg = None
+        if os.path.isfile(self.darkAVGLineEdit.text()):
+            darkavg = tf.imread(self.darkAVGLineEdit.text())
+        elif os.path.isfile(os.path.join(folder, 'dark_AVG.tif')):
+            darkavg = tf.imread(os.path.join(folder, 'dark_AVG.tif'))
+        darkstd = None
+        if os.path.isfile(self.darkSTDLineEdit.text()):
+            darkstd = tf.imread(self.darkSTDLineEdit.text())
+        elif os.path.isfile(os.path.join(folder, 'dark_STD.tif')):
+            darkstd = tf.imread(os.path.join(folder, 'dark_STD.tif'))
+
+        if not (darkavg and darkstd):
+            if not pathHasPattern(folder, '*dark*.tif*'):
+                QtW.QMessageBox.warning(self, "No dark images!",
+                    'Camera calibration requires dark images, but none provided'
+                    ' and none detected in provided folder.  Read docs for more info.',
+                    QtW.QMessageBox.Ok, QtW.QMessageBox.NoButton)
+                return
+
+        self.worker, self.thread = newWorkerThread(CamCalibWorker, folder,
+            darkavg, darkstd,
             workerConnect={
                 'progress': self.incrementProgress,
-                'setProgMax': self.progressBar.setMaximum,
+                'setProgMax': self.resetWithMax,
                 'setStatus': self.statusLabel.setText,
                 'finished': self.abortButton.hide,
             }, start=True)
-        # self.abortButton.clicked.connect(self.thread.quit)
+        # self.abortButton.clicked.connect(self._abort)
         # self.abortButton.show()
 
-    def incrementProgress(self):
+    @QtCore.pyqtSlot(int)
+    def incrementProgress(self, val=None):
         self.progressBar.setValue(self.progressBar.value() + 1)
+
+    @QtCore.pyqtSlot(int)
+    def resetWithMax(self, maxm):
+        self.progressBar.setMaximum(maxm)
+        self.progressBar.setValue(0)
+
+    @QtCore.pyqtSlot()
+    def _abort(self):
+        self.thread.terminate()
+        self.thread.wait()
 
 
 if __name__ == "__main__":
