eabe03cc31f975c6929f2dff6a6e13b8a04b6883
Talley Lambert <talley.lambert@gmail.com>
2017-11-22 13:41:27 -0500

    tools for folder sha

---
+++ tests/test_llsdir.py
@@ -0,0 +1,21 @@
+import os
+import hashlib
+
+def sha1OfFile(filepath):
+    sha = hashlib.sha1()
+    with open(filepath, 'rb') as f:
+        while True:
+            block = f.read(2**10) # Magic number: one-megabyte blocks.
+            if not block: break
+            sha.update(block)
+        return sha.hexdigest()
+
+def hash_dir(dir_path):
+    hashes = []
+    for path, dirs, files in os.walk(dir_path):
+        for file in sorted(files): # we sort to guarantee that files will always go in the same order
+            hashes.append(sha1OfFile(os.path.join(path, file)))
+        for dir in sorted(dirs): # we sort to guarantee that dirs will always go in the same order
+            hashes.append(hash_dir(os.path.join(path, dir)))
+        break # we only need one iteration - to get files and dirs in current directory
+    return str(hash(''.join(hashes)))
