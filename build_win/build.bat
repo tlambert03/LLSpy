aff9898e7b3e802a2d57cc5b30b8ba32aee56c53
Talley Lambert <talley.lambert@gmail.com>
2021-12-03 08:49:17 -0500

    remove build stuff

--- build_win/build.bat
+++
@@ -1,3 +0,0 @@
-rmdir /s /q _dist
-rmdir /s /q _build
-pyinstaller  --noconfirm --upx-dir "C:\Program Files\myapps" --log-level=INFO --distpath=./_dist --workpath=./_build --version-file=./versionInfo llspygui.spec
